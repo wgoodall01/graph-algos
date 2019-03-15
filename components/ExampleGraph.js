0243c99357c61dae3353375636348167f32eae9d
William Goodall <wgoodall01@gmail.com>
2019-03-15 17:07:41 -0400

    implement algorithm playback

--- components/ExampleGraph.js
+++ components/ExampleGraph.js
@@ -1,63 +1,95 @@
 import Graph from "../components/Graph";
 import { withId, adjacentTo, mapWhere } from "../lib/graphUtils.js";
+import { gather } from "../lib/utils.js";
 import exampleGraph from "../lib/exampleGraphData";
-
-const { nodes, edges } = exampleGraph;
+import { dijkstra } from "../lib/algos/dijkstra.js";
 
 class ExampleGraph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes,
-      edges,
+      i: 0,
+      playing: false,
+      steps: [],
       width: 500,
       height: 600
     };
   }
 
-  render() {
-    const { nodes, edges, width, height } = this.state;
-    return (
-      <Graph
-        nodes={nodes}
-        edges={edges}
-        width={width}
-        height={height}
-        draggable
-        events={{
-          click: evt => {
-            const id = evt.nodes[0];
-            if (!id) {
-              return;
-            }
-            this.setState(state => ({
-              nodes: mapWhere(state.nodes, withId(id), e => ({
-                ...e,
-                color: e.color ? null : "red"
-              })),
+  static defaultProps = {
+    graph: exampleGraph,
+    algorithm: dijkstra,
+    to: 5,
+    from: 1
+  };
 
-              edges: mapWhere(state.edges, adjacentTo(id), e => ({
-                ...e,
-                color: e.color ? null : { color: "red" }
-              }))
-            }));
-          },
-          dragEnd: evt => {
-            const nodeId = evt.nodes[0];
-            this.setState(state => ({
-              nodes: mapWhere(
-                state.nodes,
-                e => e.id == nodeId,
-                e => ({
-                  ...e,
-                  x: evt.pointer.canvas.x,
-                  y: evt.pointer.canvas.y
-                })
-              )
-            }));
-          }
-        }}
-      />
+  componentDidMount() {
+    const { graph, algorithm, to, from } = this.props;
+    const iter = algorithm(graph, 1, 5);
+
+    const { values, final } = gather(iter);
+    this.setState({ steps: values });
+  }
+
+  _handleStep = d => () => {
+    this.setState(e => {
+      let x = e.i + d;
+      if (x >= 0 && x < e.steps.length) {
+        return { i: x };
+      } else {
+        return { playing: false };
+      }
+    });
+  };
+
+  _handlePlay = dt => () => {
+    this.setState({ playing: dt || false });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { playing } = this.state;
+    if (playing && (prevState.i !== this.state.i || !prevState.playing)) {
+      setTimeout(this._handleStep(playing > 0 ? 1 : -1), playing);
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._interval);
+  }
+
+  render() {
+    const { steps, i, width, height, playing } = this.state;
+    const thisStep = steps[i];
+    return (
+      <div>
+        <button onClick={() => this.setState({ i: 0 })}>{0}</button>
+        <button onClick={this._handleStep(-1)}>{"<"}</button>
+        <input
+          type="range"
+          step={1}
+          min={0}
+          max={steps.length - 1}
+          value={i}
+          onChange={e => this.setState({ i: parseInt(e.target.value) })}
+        />
+        <button onClick={this._handleStep(1)}>{">"}</button>
+        {playing ? (
+          <button onClick={this._handlePlay(0)}>{"x"}</button>
+        ) : (
+          <button onClick={this._handlePlay(200)}>{">>"}</button>
+        )}
+        <span>
+          step {i + 1} of {steps.length}, playing={playing.toString()}
+        </span>
+        {thisStep && (
+          <Graph
+            nodes={thisStep.nodes}
+            edges={thisStep.edges}
+            width={width}
+            height={height}
+          />
+        )}
+      </div>
     );
   }
 }
